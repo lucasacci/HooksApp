c26d446a3c3a6159d4d580eca2760663dd9c0499
Luciano <lucasacci@hotmail.com>
2022-10-17 14:39:15 -0300

    use counter done

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { HooksApp } from './HooksApp'
 import './index.css'
 import { CounterApp } from './useState/CounterApp'
+import { CounterCustomHook } from './useState/CounterCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp/>
+    <CounterCustomHook/>
   </React.StrictMode>
 )
--- src/useState/CounterApp.jsx
+++ src/useState/CounterApp.jsx
@@ -2,16 +2,27 @@ import { useState } from "react"
 
 export const CounterApp = () => {
 
-    const [counter, setCounter] = useState(10)
+    const [counter, setCounter] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30
+    })
+
+      const {counter1, counter2, counter3} = counter
 
 
   return (
     <>
-        <h1>Counter: {counter}</h1>
+        <h1>Counter: {counter.counter1}</h1>
+        <h1>Counter: {counter.counter2}</h1>
+        <h1>Counter: {counter.counter3}</h1>
 
         <hr />
 
-        <button onClick={()=> setCounter(counter+1)}>+1</button>
+        <button className="btn btn-outline-danger" onClick={()=> setCounter({
+          ...counter,
+          counter1: counter1 + 1
+        })}>+1</button>
     </>
   )
 }
