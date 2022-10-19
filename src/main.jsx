1da207d4bd3bf82a33b05a8ccf440ad297dd6d57
Luciano <lucasacci@hotmail.com>
2022-10-19 16:21:17 -0300

    useMemo - useCallback example

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Padre } from './07-tarea-memo/Padre'
 import { MultipleCustomHooks } from './examples/MultipleCustomHooks'
 import { HooksApp } from './HooksApp'
 import './index.css'
@@ -15,6 +16,6 @@ import { CounterCustomHook } from './useState/CounterCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <CallbackHook/>
+    <Padre/>
   // </React.StrictMode>
 )
